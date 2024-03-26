7446d991e0efb3e844dbf6b55fcfd8eda828ea36
TAHA SELLAK <sellaktaha1@gmail.com>
2024-03-26 21:47:04 +0000

    navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function activateItem(item) {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+        } else {
+            item.classList.add('active');
+        }
+    }
+    
+
