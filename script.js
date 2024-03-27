1837ef18759fe01291852c4fb61a9901c4cad557
TAHA SELLAK <sellaktaha1@gmail.com>
2024-03-27 00:24:06 +0000

    Search Input HTML&CSS et Demo Carte

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const input = document.querySelector(".input-container");
 
 function activateItem(item) {
         if (item.classList.contains('active')) {
             item.classList.remove('active');
+            input.classList.remove('active');
         } else {
             item.classList.add('active');
+            input.classList.add('active');
         }
     }
     
