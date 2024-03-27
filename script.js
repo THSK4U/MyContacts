5af889c71c485b4b634d342b7577d3511f96393d
TAHA SELLAK <sellaktaha1@gmail.com>
2024-03-27 14:31:39 +0000

    Lier API aux cards

--- script.js
+++ script.js
@@ -9,5 +9,24 @@ function activateItem(item) {
             input.classList.add('active');
         }
     }
-    
+
+
+// fetch("https://randomuser.me/api/?page=3&results=10&seed=abc").then(function(response){
+//     console.log(response[0].gender)
+//     return response.json;
+// })
+
+async function get(){
+    const response = await fetch("https://randomuser.me/api/?nat=fr")
+    const data = await response.json()
+    const user = data.results[0]
+console.log(data)
+    document.querySelector(".titlefront").innerHTML = user.name.first + ' ' + user.name.last;
+    document.querySelector(".titleback").innerHTML = user.name.first + ' ' + user.name.last;
+    document.querySelector(".email").innerHTML = user.email;
+    document.querySelector(".phone").innerHTML = user.phone;
+    document.querySelector('.flip-card-front').style.backgroundImage = `url('${user.picture.large}')`;
+}
+
+get()
 
