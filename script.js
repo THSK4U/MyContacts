9a1cb162c2d3ca5b74cc1b7f4440c456835d0968
TAHA SELLAK <sellaktaha1@gmail.com>
2024-03-29 15:40:54 +0000

    function boucle & json & creation HTML ajouter

--- script.js
+++ script.js
@@ -16,17 +16,37 @@ function activateItem(item) {
 //     return response.json;
 // })
 
-async function get(){
-    const response = await fetch("https://randomuser.me/api/?nat=fr")
-    const data = await response.json()
-    const user = data.results[0]
-console.log(data)
-    document.querySelector(".titlefront").innerHTML = user.name.first + ' ' + user.name.last;
-    document.querySelector(".titleback").innerHTML = user.name.first + ' ' + user.name.last;
-    document.querySelector(".email").innerHTML = user.email;
-    document.querySelector(".phone").innerHTML = user.phone;
-    document.querySelector('.flip-card-front').style.backgroundImage = `url('${user.picture.large}')`;
+async function get() {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+        console.log(data)
+
+    const cardsContainer = document.querySelector('.cards');
+    data.results.forEach(user => {
+        const flipcardElement = document.createElement("div");
+        flipcardElement.classList.add("flipcard");
+        flipcardElement.innerHTML = `
+            <div class="flip-card-inner">
+                <div class="flip-card-front"  style="background-image: url('${user.picture.large}')">
+                    <p class="titlefront">${user.name.first} ${user.name.last}</p>
+                </div>
+                <div class="flip-card-back">
+                    <div class="image" style="background-image: url('${user.picture.large}')"></div>
+                    <p class="titleback">${user.name.first} ${user.name.last}</p>
+                    <p class="description">${user.location.street.name}</p>
+                    <p class="email">${user.email}</p>
+                    <p class="phone">${user.phone}</p>
+                </div>
+            </div>
+        `;
+        cardsContainer.appendChild(flipcardElement);
+    });
 }
 
-get()
+function boucle(){
+    for (i = 0 ;i<50;i++){
+         get();
+    }
+}
 
+boucle()
